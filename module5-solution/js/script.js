7c0ae83212c1740df78c4529af5b7c630c0356f7
Vernon Cole <vernoncole@provalus.com>
2023-09-29 09:35:04 -0500

    some updates git push

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -117,14 +117,16 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(html, "category", category);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName","'" + chooseRandomCategory + "'");
 
-
+      console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
