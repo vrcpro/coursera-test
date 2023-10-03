6ba8a2e1752c8ea4604196c44950e1d0c681a353
Vernon Cole <vernoncole@provalus.com>
2023-10-03 16:26:28 -0500

    assignment 2 stuff

---
+++ Ang/assignment2/app.js
@@ -0,0 +1,74 @@
+(function (){
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    listCheckOff.$inject['$scope','$filter', '$injector'];
+
+
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    
+    function ToBuyController(ShoppingListCheckOffService){
+        var toBuy = this;
+
+        toBuy.items = ShoppingListCheckOffService.getItems();
+    //    toBuy.moveToBought = ShoppingListCheckOffService.moveToBought(itemIndex);
+        toBuy.removeItem = function (itemIndex){
+            ShoppingListCheckOffService.removeItem(itemIndex);
+        }
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+        var alreadyBought = this;
+
+        alreadyBought.items = ShoppingListCheckOffService.getBought();
+        alreadyBought.removeItem = function (itemIndex){
+            ShoppingListCheckOffService.removeItem(itemIndex);
+        };
+        
+    }
+
+    function ShoppingListCheckOffService(){
+        var service = this;
+        
+        var toBuyItems = [
+            {name: 'apple', quantity: 12},
+            {name: 'pizza', quantity: 22},
+            {name: 'sushi', quantity: 2},
+            {name: 'chips', quantity: 112},
+            {name: 'tots', quantity: 1}
+
+    
+    ];
+        var alreadyBoughtItems = [];
+        
+        //showList.toBuy =ShoppingListCheckOffService.toBuy;
+        //showList.alreadyBought = ShoppingListCheckOffService.alreadyBought;
+        
+        service.getItems = function () {
+            return toBuyItems;
+        }
+
+        service.getBought = function () {
+            return alreadyBoughtItems;
+        }
+
+        service.removeItem = function (itemIndex) {
+            alreadyBoughtItems.push(toBuyItems[itemIndex]);
+            toBuyItems.splice(itemIndex, 1);
+        }
+
+     //   service.addItemToBought (item.name, item.quantity){
+      //      return 
+     //   }
+    
+    }
+
+
+
+
+})();
